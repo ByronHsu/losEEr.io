9ddfd4841781a000ac22b290b28b5a6aceb6b090
shenperson <wayne19990609@gmail.com>
2018-09-22 00:03:17 +0800

    generate food & food balance

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var unique = require('node-uuid');
 var uuid = require('uuid/v4');
 var app = express();
 var serv = require('http').Server(app);
-app.get('/',function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(__dirname, 'public/index.html');
 });
 
@@ -19,7 +19,7 @@ const Util = {
      * @param  {Integer} max Maximum of range
      * @return {Integer}     random number generated
      */
-    randomInt: function(min, max) {
+    randomInt: function (min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -31,27 +31,28 @@ const Util = {
      * @param  {Number} x2 second point
      * @param  {Number} y2 second point
      */
-    distanceFormula: function(x1, y1, x2, y2) {
-        var withinRoot = Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2);
+    distanceFormula: function (x1, y1, x2, y2) {
+        var withinRoot = Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
         var dist = Math.pow(withinRoot, 0.5);
         return dist;
     }
 };
 // io connection 
-const width = 500;
-const height = 500;
-const cornerWidth=110;
+const width = 2000;
+const height = 2000;
+const cornerWidth = 110;
 const cornerHeight = 110;
-var io = require('socket.io')(serv,{});
+const limitFoodAmount = 1000;
+var io = require('socket.io')(serv, {});
 var snakeArr = [];
 var foodArr = [];
-var foodAmount = 200;
-for(var i=0; i<foodAmount; i++){
-    foodArr.push({id: uuid(), x: Util.randomInt(-width + cornerWidth ,width - cornerWidth), y: Util.randomInt(-height + cornerHeight ,height - cornerHeight)});
+var foodAmount = 10;
+for (var i = 0; i < foodAmount; i++) {
+    foodArr.push({ id: uuid(), x: Util.randomInt(-width + cornerWidth, width - cornerWidth), y: Util.randomInt(-height + cornerHeight, height - cornerHeight) });
 }
 let socketToSnakeID = {}
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
     // console.log(socket.id)
     socket.on('createPlayer', (data) => {
         console.log('createPlayer', data.id);
@@ -64,24 +65,25 @@ io.sockets.on('connection', function(socket) {
         //   console.log(typeof(socket.id))
         socketToSnakeID[socket.id] = data.id
         console.log(socketToSnakeID)
-            //send message to every connected client except the sender
+        //send message to every connected client except the sender
         socket.broadcast.emit('new_enemyPlayer', data);
     });
     socket.on('playerMove', data => {
         var snake = snakeArr.find(e => e.id == data.id);
-        if(snake == null) return;
+        if (snake == null) return;
         snake.headPath = data.headPath;
         snake.headAngle = data.headAngle;
         //   console.log("playerMove", data)
         socket.broadcast.emit('enemyMove', data);
     });
-    socket.on('on_food_init', function() {
+    socket.on('on_food_init', function () {
         socket.emit('on_get_food', foodArr);
     });
-    socket.on('food_destroy', function(id) {
+    socket.on('food_destroy', function (id) {
         console.log(`Received request to destroy food id=${id} @ app.js:anonymous/food_destroy`)
-        for(var i = 0; i < foodArr.length; i++) {
-            if(foodArr[i].id === id) {
+        foodAmount--;
+        for (var i = 0; i < foodArr.length; i++) {
+            if (foodArr[i].id === id) {
                 foodArr.splice(i, 1);
                 break;
             }
@@ -102,7 +104,7 @@ io.sockets.on('connection', function(socket) {
             }
         }
         console.log(`Snake ${data.id} died @ app.js: anonymous/snakeDestroyed`)
-            // console.log('Received id and foodDrop @ app.js: anonymous/snakeDestroyed');
+        // console.log('Received id and foodDrop @ app.js: anonymous/snakeDestroyed');
         foodArr.push(...data.drop); // Otherwise the newly added snake won't see food dropped from dead snake
         foodAmount += data.drop.length;
         socket.broadcast.emit('enemyDestroy', data.id, data.drop);
@@ -111,7 +113,7 @@ io.sockets.on('connection', function(socket) {
     socket.on("playerIncrease", data => {
         let snake = snakeArr.find((e) => e.id == data.id);
         if (snake == null) return
-            // console.log("playerIncrease", data)
+        // console.log("playerIncrease", data)
         snake.scale = data.scale
         snake.snakeLength = data.snakeLength
         snake.headAngle = data.headAngle
@@ -127,7 +129,7 @@ io.sockets.on('connection', function(socket) {
 
     socket.on("disconnect", () => {
         let snakeId = socketToSnakeID[socket.id]
-            //    console.log("user disconnect", snakeId)
+        //    console.log("user disconnect", snakeId)
         if (!snakeId) return;
         for (let i = 0; i < snakeArr.length; i++) {
             if (snakeArr[i].id === snakeId) {
@@ -140,3 +142,15 @@ io.sockets.on('connection', function(socket) {
         socket.broadcast.emit('enemyDisconnect', snakeId)
     })
 });
+function genfood() {
+    console.log(foodAmount);
+    var newfoods = [];
+    while (foodAmount < limitFoodAmount) {
+        foodAmount++;
+        var newfood = { id: uuid(), x: Util.randomInt(-width + cornerWidth, width - cornerWidth), y: Util.randomInt(-height + cornerHeight, height - cornerHeight) }
+        newfoods.push(newfood);
+        foodArr.push(newfood);
+    }
+    io.emit('on_get_food', newfoods);
+}
+setInterval(genfood, 2000);
